2d5eaaba56f50098252fcc33d98f661e3c512a97
brenu <bvsousa64@gmail.com>
2020-09-18 12:28:45 -0300

    :rocket: Create sample card

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Main() {
   return (
     <View style={styles.container}>
-      <Text>Sample text here for the main page</Text>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Texto de teste aqui</Text>
+        <Text style={styles.cardContent}>
+          O leão-marinho é um mamífero semiaquático, de que há várias espécies,
+          da subfamília Otariinae da família Otariidae, que vive em regiões de
+          baixas temperaturas e alimenta-se principalmente de peixes (como o
+          cherne e o arenque) e de moluscos.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +20,28 @@ export default function Main() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#7ec0ee",
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 30,
+  },
+  card: {
+    alignSelf: "stretch",
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    padding: 30,
+    borderRadius: 5,
+    elevation: 5,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#777",
+  },
+  cardContent: {
+    marginTop: 10,
+    fontSize: 14,
+    color: "#777",
   },
 });
