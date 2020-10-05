21636290469ab6ec4e508d1bd100bd000085020c
brenu <bvsousa64@gmail.com>
2020-10-05 12:56:37 -0300

    :art: Begin to implement color picker for the settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Modal,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TriangleColorPicker } from "react-native-color-picker";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Settings() {
   const navigation = useNavigation();
@@ -20,6 +28,19 @@ export default function Settings() {
       </View>
       <View style={styles.content}>
         <Text style={styles.title}>Configurações</Text>
+        <Text style={styles.label}>Cor principal</Text>
+        <TouchableOpacity style={styles.btn}>
+          <Text>Selecionar</Text>
+        </TouchableOpacity>
+        <Modal style={styles.modal} transparent={true}>
+          <View style={styles.modalCard}>
+            <Text style={styles.title}>Selecione a cor</Text>
+            <TriangleColorPicker
+              onColorSelected={(color) => alert(`Color selected: ${color}`)}
+              style={{ flex: 0.8 }}
+            />
+          </View>
+        </Modal>
       </View>
     </View>
   );
@@ -48,5 +69,30 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#7ec0ee",
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "normal",
+    color: "#7ec0ee",
+  },
+  input: {
+    height: 50,
+    padding: 5,
+    paddingBottom: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: "#7ec0ee",
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  modalCard: {
+    flex: 0.8,
+    margin: 10,
+    padding: 20,
+    borderRadius: 5,
+    elevation: 5,
+    backgroundColor: "#fff",
   },
 });
