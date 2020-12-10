9e6786019aa214c73e742cfb911d82c1d25a2c83
brenu <bvsousa64@gmail.com>
2020-12-10 07:00:30 -0300

    :art: Prevent splash screen from auto hiding

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
 import Routes from "./src/routes";
 
 export default function App() {
+  useEffect(() => {
+    async function handleInit() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    handleInit();
+  }, []);
+
   return (
     <>
       <Routes />
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -7,14 +7,43 @@ import Main from "./pages/Main";
 import Editor from "./pages/Editor";
 import NewMemo from "./pages/NewMemo";
 import Settings from "./pages/Settings";
+import { AsyncStorage } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
+  const [hasSettings, setHasSettings] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function handleInit() {
+      let settings = await AsyncStorage.getItem("settings");
+      settings = JSON.parse(settings);
+
+      if (settings) {
+        setHasSettings(true);
+      }
+      setIsLoaded(true);
+    }
+
+    handleInit();
+  }, []);
+
+  useEffect(() => {
+    async function handleSplashHiding() {
+      await SplashScreen.hideAsync();
+    }
+
+    if (isLoaded) {
+      handleSplashHiding();
+    }
+  }, [isLoaded]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Landing" component={Landing} />
+        {!hasSettings && <Stack.Screen name="Landing" component={Landing} />}
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="NewMemo" component={NewMemo} />
         <Stack.Screen name="Editor" component={Editor} />
